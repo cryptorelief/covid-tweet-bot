55a15650fb10a3e65fbb40fd72a381ae7e25fe74
Ananthu Kanive <ananthu.kanive@smallcase.com>
2021-05-10 20:49:32 +0530

    fix bot replying to bot

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const creds = {
   access_token_secret: process.env["access_token_secret"],
 };
 
+const botUserId = process.env["bot_user_id"];
+
 const twitter = new Twit(creds);
 
 const replyToTweet = async (parentTweetId, replyText) => {
@@ -21,18 +23,21 @@ const twitterStream = twitter.stream("statuses/filter", {
   track: ["@IntrobotAI", "#IntrobotAI"], // any relavant words, hashtags, mentions
 });
 
-twitterStream.on("tweet", processTweet);
-
-const processTweet = (tweet) => {
+const processTweet = async (tweet) => {
   const tweetId = tweet?.id_str;
   const tweetUser = tweet?.user?.screen_name;
   const tweetText = tweet?.text;
 
-  if (tweetText) {
+  if (tweetText && tweetUser !== botUserId) {
     // process text and determine if the bot has to reply
     // ...
 
     // implement maybe a throttled queue here if we get too many tweets
-    await replyToTweet(tweetId, `Hello ${tweetUser}, visit example.com for more`);
+    await replyToTweet(
+      tweetId,
+      `Hello ${tweetUser}, visit https://covidcitizens.org for more`
+    );
   }
 };
+
+twitterStream.on("tweet", processTweet);
