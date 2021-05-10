d85196b1eb7e5d7995fa2fe80211edba146b06cf
Ananthu Kanive <ananthu.kanive@smallcase.com>
2021-05-11 00:41:33 +0530

    add config files and move tweet related functions to a file

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const Twit = require("twit");
-
-const creds = {
-  consumer_key: process.env["consumer_key"],
-  access_token: process.env["access_token"],
-  consumer_secret: process.env["consumer_secret"],
-  access_token_secret: process.env["access_token_secret"],
-};
+const fs = require("fs");
+const Twitter = require("./src/twitter");
 
 const botUserId = process.env["bot_user_id"];
-
-const twitter = new Twit(creds);
-
-const replyToTweet = async (parentTweetId, replyText) => {
-  return twitter.post("statuses/update", {
-    status: replyText,
-    in_reply_to_status_id: parentTweetId,
-    auto_populate_reply_metadata: true,
-  });
-};
-
-const twitterStream = twitter.stream("statuses/filter", {
-  track: ["@IntrobotAI", "#IntrobotAI"], // any relavant words, hashtags, mentions
-});
+const startConfig = require("./startConfig.json");
 
 const processTweet = async (tweet) => {
   const tweetId = tweet?.id_str;
-  const tweetUser = tweet?.user?.screen_name;
   const tweetText = tweet?.text;
+  const tweetUserId = tweet?.user?.id_str;
 
-  if (tweetText && tweetUser !== botUserId) {
+  if (tweetUserId === botUserId) {
+    // don't process tweets from the bot itself
+    return;
+  }
+
+  try {
+    const raw = fs.readFileSync("./runConfig.json").toString();
+    const runConfig = JSON.parse(raw);
+
+    const dateDiff = new Date(runConfig?.startTime) - new Date();
+    if (runConfig?.enabled === false || dateDiff > 0) {
+      return;
+    }
+  } catch (error) {
+    console.log("error reading config, assuming active");
+  }
+
+  if (tweetText) {
     // process text and determine if the bot has to reply
     // ...
 
     // implement maybe a throttled queue here if we get too many tweets
-    await replyToTweet(
-      tweetId,
-      `Hello ${tweetUser}, visit https://covidcitizens.org for more`
-    );
+    await replyToTweet(tweetId, startConfig.reply);
   }
 };
 
-twitterStream.on("tweet", processTweet);
+Twitter.listenToStream(startConfig.monitorTracks, (tweet) => {
+  try {
+    processTweet(tweet);
+  } catch (error) {
+    // ideally must send an alert to the maintainers
+    console.error(error);
+  }
+});
---
+++ src/twitter.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const Twit = require("twit");
+
+const creds = {
+  consumer_key: process.env["consumer_key"],
+  access_token: process.env["access_token"],
+  consumer_secret: process.env["consumer_secret"],
+  access_token_secret: process.env["access_token_secret"],
+};
+
+const twitterInstance = new Twit(creds);
+
+/**
+ * retpy to a tweet
+ *
+ * @param {string} parentTweetId
+ * @param {string} replyText
+ */
+const replyToTweet = async (parentTweetId, replyText) => {
+  twitterInstance.post("statuses/update", {
+    status: replyText,
+    in_reply_to_status_id: parentTweetId,
+    auto_populate_reply_metadata: true,
+  });
+};
+
+/**
+ * listen to a given tweet stream
+ *
+ * @param {string[]} track
+ * @param {() => void} cb
+ */
+const listenToStream = (track, cb) => {
+  const twitterStream = twitterInstance.stream("statuses/filter", { track });
+
+  twitterStream.on("connect", () => {
+    console.log("stream - ", track);
+    console.log("connecting ...");
+  });
+
+  twitterStream.on("connected", () => {
+    console.log("stream - ", track);
+    console.log("connected");
+  });
+
+  twitterStream.on("tweet", cb);
+};
+
+module.exports = {
+  replyToTweet,
+  listenToStream,
+  twitterInstance,
+};
